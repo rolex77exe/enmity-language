724272a29c5c98f6d139c2f76d455aeebe58dd32
rolex77exe <rolex77exe@gmail.com>
2026-05-01 14:43:08 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,47 +1,76 @@
-(function(plugins,Patcher,metro,common){'use strict';function _interopNamespace(e){if(e&&e.__esModule)return e;var n=Object.create(null);if(e){Object.keys(e).forEach(function(k){if(k!=='default'){var d=Object.getOwnPropertyDescriptor(e,k);Object.defineProperty(n,k,d.get?d:{enumerable:true,get:function(){return e[k]}});}})}n["default"]=e;return Object.freeze(n)}var Patcher__namespace=/*#__PURE__*/_interopNamespace(Patcher);const X1 = "krd";
+(function(plugins){'use strict';const X1 = "krd";
 const X2 = "1978";
+// Mobil uyumlu Base64
+const b64chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/';
+function encodeB64(input) {
+    let str = input;
+    let output = '';
+    for (let block = 0, charCode, i = 0, map = b64chars; str.charAt(i | 0) || (map = '=', i % 1); output += map.charAt(63 & block >> 8 - i % 1 * 8)) {
+        charCode = str.charCodeAt(i += 3 / 4);
+        block = block << 8 | charCode;
+    }
+    return output;
+}
 function encodeSecret(input) {
-    const textEncoder = new TextEncoder();
-    const key = textEncoder.encode(X2);
-    const data = textEncoder.encode(input);
-    const result = new Uint8Array(data.length);
-    for (let i = 0; i < data.length; i++)
-        result[i] = data[i] ^ key[i % key.length];
-    let binary = "";
-    for (let i = 0; i < result.length; i++)
-        binary += String.fromCharCode(result[i]);
-    return `${X1}${btoa(binary).replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '')}`;
+    let result = "";
+    for (let i = 0; i < input.length; i++) {
+        result += String.fromCharCode(input.charCodeAt(i) ^ X2.charCodeAt(i % X2.length));
+    }
+    const b64 = encodeB64(result);
+    return `${X1}${b64.replace(/\+/g, '-').replace(/\//g, '_').replace(/=+$/g, '')}`;
 }
 const SecretLanguagePlugin = {
-    name: 'EnmitySecret',
-    description: 'Gizli Dil (En Stabil Sürüm)',
-    version: '2.0.0',
+    name: 'SecretLanguage',
+    description: 'Gizli Dil (Ultra Kararlı Sürüm)',
+    version: '2.3.0',
     authors: [{ name: 'You', id: '0' }],
     onStart() {
-        try {
-            const MessageActions = metro.getByProps('sendMessage');
-            if (MessageActions) {
-                Patcher__namespace.before('EnmitySecret', MessageActions, 'sendMessage', (_self, args) => {
-                    const [, message] = args;
-                    if (message && message.content && !message.content.startsWith(X1)) {
-                        message.content = encodeSecret(message.content);
-                    }
-                });
-                // Başlangıç bildirimi
-                if (common.Toasts && common.Toasts.open) {
-                    common.Toasts.open({ content: "Gizli Dil Aktif!" });
+        // Discord'un modülleri tam yüklemesi için 3 saniye bekle
+        setTimeout(() => {
+            try {
+                const e = window.enmity;
+                if (!e)
+                    return;
+                // Modül bulucu: Her yeri tek tek dener
+                const getModule = () => {
+                    if (e.metro && e.metro.getByProps)
+                        return e.metro;
+                    if (e.modules && e.modules.common && e.modules.common.getByProps)
+                        return e.modules.common;
+                    if (e.getByProps)
+                        return e;
+                    return null;
+                };
+                const metro = getModule();
+                const patcher = e.patcher;
+                const toasts = e.modules?.common?.Toasts || e.toasts;
+                if (!metro || !patcher) {
+                    if (toasts)
+                        toasts.open({ content: "HATA: Gerekli modüller bulunamadı!" });
+                    return;
+                }
+                const MessageActions = metro.getByProps('sendMessage');
+                if (MessageActions) {
+                    patcher.before('SecretLanguage', MessageActions, 'sendMessage', (_self, args) => {
+                        const [, message] = args;
+                        if (message && message.content && !message.content.startsWith(X1)) {
+                            message.content = encodeSecret(message.content);
+                        }
+                    });
+                    if (toasts)
+                        toasts.open({ content: "SecretLanguage Başarıyla Yüklendi! (Mesajlar Şifrelenecek)" });
                 }
             }
-            else {
-                console.log("EnmitySecret: MessageActions bulunamadı");
+            catch (err) {
+                console.log("SecretLanguage Başlatma Hatası:", err);
             }
-        }
-        catch (err) {
-            console.error("EnmitySecret Hata:", err);
-        }
+        }, 3000);
     },
     onStop() {
-        Patcher__namespace.unpatchAll('EnmitySecret');
+        const e = window.enmity;
+        if (e && e.patcher && e.patcher.unpatchAll) {
+            e.patcher.unpatchAll('SecretLanguage');
+        }
     }
 };
-plugins.registerPlugin(SecretLanguagePlugin);})(enmity.plugins,enmity.patcher,enmity.metro,enmity.modules.common);
+plugins.registerPlugin(SecretLanguagePlugin);})(plugins);
